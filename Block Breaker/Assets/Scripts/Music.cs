bcb3229802e7ab90ca60b72ad495041cafeffb28
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-01-20 14:08:59 -0500

    Started the block breaker game in unity

---
+++ Block Breaker/Assets/Scripts/Music.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+
+	static Music instance = null;
+	// Use this for initialization
+	
+	void Awake(){
+		Debug.Log("Music Player Awake " + GetInstanceID());
+	}
+	
+	void Start () {
+		
+		Debug.Log("Music Player Starts " + GetInstanceID() );
+		if(instance != null){
+			Destroy(gameObject);
+			print ("Duplicate music player destroyed!!");
+		}else{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Block Breaker/Assets/Scripts/levelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelManager : MonoBehaviour {
+
+	public void loadLevel(string name){
+		Debug.Log("Level Load Requested: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void quitRequest(string name){
+		Debug.Log("Level quit request: " + name);
+		Application.Quit();
+	}
+}
