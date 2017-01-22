86e3584288623a45351c1878e6948c3b3151729f
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-01-22 18:11:10 -0500

    Added block and Paddle

---
+++ Block Breaker/Assets/Scripts/ball.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ball : MonoBehaviour {
+
+	public paddle pad; 
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - pad.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	if(!hasStarted){
+			this.transform.position = pad.transform.position + paddleToBallVector;
+			
+			if(Input.GetMouseButtonDown(0)){
+				//print ("Mouse Clciked!!");
+				hasStarted = true;
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+			}
+		}
+	}
+}
