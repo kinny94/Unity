8792389f86d92b78f9f485847dcef5dfef3fe2b9
kiiiiNNNNNNNNNyyyy <ad.sam007@gmail.com>
2017-01-23 15:37:27 -0500

    Half way done

---
+++ Block Breaker/Assets/Scripts/brick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class brick : MonoBehaviour {
+
+	public int maxHits = 1;
+	private int timesHit;
+
+	// Use this for initialization
+	void Start () {
+		timesHit = 0;
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision){
+		timesHit++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
